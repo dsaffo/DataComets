015fdf952bcd482ab0a86da3d46fedd6ccb07d38
David Saffo <dsaffo@luc.edu>
2019-07-25 16:27:58 -0400

    scroll to top and modal

--- main.js
+++ main.js
@@ -22,6 +22,18 @@ let dimensions = {
 
 console.log(dimensions);
 
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.modal');
+  var instances = M.Modal.init(elems, {});
+});
+
+let topButton = document.getElementById('top');
+
+top.addEventListener('click', function(){
+  console.log('hello');
+    //document.getElementById('tree-container').scrollTop = 0; // For Safari
+    document.getElementById('tree-container').scrollTop = 0; // For Chrome, Firefox, IE and Opera
+});
 
 
 d3.json('data/data.json').then(function (data) {
@@ -56,21 +68,21 @@ d3.json('data/data.json').then(function (data) {
       y: 'control[3]',
       title: 'Thrust (0=no thrust - 1=max thrust)'
     },
-      {
+    {
       data: data['actuator_controls_0'],
       log: 'actuator_controls_0',
       x: 'timestamp',
       y: 'control[0]',
       title: 'Roll (-1 - 1)'
     },
-      {
+    {
       data: data['actuator_controls_0'],
       log: 'actuator_controls_0',
       x: 'timestamp',
       y: 'control[1]',
       title: 'Pitch (-1 - 1)'
     },
-      {
+    {
       data: data['actuator_controls_0'],
       log: 'actuator_controls_0',
       x: 'timestamp',
@@ -98,7 +110,7 @@ d3.json('data/data.json').then(function (data) {
       y: 'current_filtered_a',
       title: 'Battery Current (Amps)'
     },
-     {
+    {
       data: data['battery_status'],
       log: 'battery_status',
       x: 'timestamp',
@@ -119,16 +131,16 @@ d3.json('data/data.json').then(function (data) {
       y: 'jamming_indicator',
       title: 'Jamming Indicator'
     },
-      {
+    {
       data: data['cpuload'],
       log: 'cpuload',
       x: 'timestamp',
       y: 'load',
       title: 'CPU Load (0 - 1)'
     }
-    
-    
-  ] 
+
+
+  ]
 
   const map = Map(dispatch, data, dimensions);
   const timeline = Timeline(dispatch, data, dimensions);
